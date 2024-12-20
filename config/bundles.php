31e26cd66fa2363379310c14a8d984b600688700
Jason <rutyo@mail.ru>
2024-12-20 05:40:36 -0900

    create and configurate bundle for admin functionality

--- config/bundles.php
+++ config/bundles.php
@@ -1,8 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Jason\CoreBundle\CoreBundle::class => ['all' => true],
+    Jason\AdminBundle\AdminBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
